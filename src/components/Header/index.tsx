dc98f38b90d14c08bbd0948ff008767a531cb010
mawburn <mawburn7@gmail.com>
2020-03-19 20:17:45 -0400

    basic changes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react'
+
+import s from './styles.module.scss'
+
+const Header = () => <div className={s.header} />
+
+export default Header
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import s from './styles.module.scss'
+
+const Hero = () => <div className={s.hero} />
+
+export default Hero
