608a2c1ca85ceaf0e2a4c442b2b1281f4af7b750
mawburn <mawburn7@gmail.com>
2023-05-04 19:48:33 -0400

    fix external links in markdown

--- src/components/MDXLoader.tsx
+++ src/components/MDXLoader.tsx
@@ -1,5 +1,6 @@
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import Image from 'next/image'
+import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 
 import { Header } from './Header'
@@ -7,6 +8,27 @@ import { Icon } from './Icon'
 import { NoBreak } from './NoBreak'
 
 export type MDXResult = MDXRemoteSerializeResult<Record<string, unknown>, Record<string, unknown>>
+
+const Anchor = ({ children, ...props }: PropsWithChildren<any>) => {
+  const { href, target, rel, ...rest } = props
+
+  if (!href) {
+    return children
+  }
+
+  const isExternal = href.startsWith('http')
+
+  return (
+    <Link
+      href={href}
+      target={isExternal ? '_blank' : target}
+      rel={isExternal ? 'noopener noreferrer' : rel}
+      {...rest}
+    >
+      {children}
+    </Link>
+  )
+}
 
 interface Props {
   source: MDXResult
@@ -16,6 +38,7 @@ const components = {
   h1: Header.H1,
   h2: Header.H2,
   h3: Header.H3,
+  a: Anchor,
   Image: Image,
   Icon: Icon,
   NoBreak: NoBreak,
