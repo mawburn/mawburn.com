f814342fb6744d367e722d6f28bb46645f4b5fa3
Matt Burnett <matt.burnett@shopify.com>
2025-04-03 14:07:57 -0400

    remove next

--- src/components/Icon.tsx
+++
@@ -1,7 +0,0 @@
-interface IconProps {
-  name: string
-}
-
-export const Icon = ({ name }: IconProps) => (
-  <span className={`icon-${name.toLowerCase()}`} title={name} />
-)
--- src/components/Layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-interface FooterProps {}
-
-export const Footer = ({}: FooterProps) => (
-  <footer className="bottom-0 text-center text-sm	py-4 px-0">
-    &copy; {new Date().getFullYear()} mawburn.com
-  </footer>
-)
